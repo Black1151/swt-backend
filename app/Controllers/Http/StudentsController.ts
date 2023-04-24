8925acce338a1947886ce596f5df8306feffc2a9
blackPrawn <danblack9988@gmail.com>
2023-04-24 14:17:31 +0100

    add new endpoints to Students controller for creating and updating goals and adding evidence.

--- app/Controllers/Http/StudentsController.ts
+++ app/Controllers/Http/StudentsController.ts
@@ -1,5 +1,5 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Student from 'App/Models/Student'
+import Student, { Goal, Evidence } from 'App/Models/Student'
 
 export default class StudentsController {
 
@@ -15,12 +15,88 @@ export default class StudentsController {
 
   public async create({}: HttpContextContract) {}
 
-  public async store({ request, response }: HttpContextContract) {
-    
-    console.log(request.body())
+  // Add a new goal for a student
+public async addGoal({ params, request, response }: HttpContextContract) {
+  try {
+    const student = await Student.find(params.id)
+    if (student) {
+      const newGoal : Goal = request.only(['title', 'date', 'status', 'evidence'])
+      student.goals.push(newGoal) // Use goals directly as an array
+      await student.save()
+      response.status(200).send(student)
+    } else {
+      response.status(404).send({ message: 'Student not found.' })
+    }
+  } catch (error) {
+    console.error(error)
+    response.status(500).send({ message: 'Error adding goal.' })
+  }
+}
 
+// Update the status of a goal for a student
+public async updateGoalStatus({ params, request, response }: HttpContextContract) {
+  try {
+    const student = await Student.find(params.id)
+    const goalIndex = request.input('goal_index')
+    const newStatus = request.input('status')
+
+    if (student) {
+      if (student.goals[goalIndex]) { // Use goals directly as an array
+        student.goals[goalIndex].status = newStatus
+        await student.save()
+        response.status(200).send(student)
+      } else {
+        response.status(404).send({ message: 'Goal not found.' })
+      }
+    } else {
+      response.status(404).send({ message: 'Student not found.' })
+    }
+  } catch (error) {
+    console.error(error)
+    response.status(500).send({ message: 'Error updating goal status.' })
+  }
+}
+
+// Add an entry to the evidence for a goal
+public async addEvidence({ params, request, response }: HttpContextContract) {
+  try {
+    const student = await Student.find(params.id)
+    const goalIndex = request.input('goal_index')
+    const newEvidence : Evidence = request.only(['date', 'name', 'image_link'])
+
+    if (student) {
+      if (student.goals[goalIndex]) { 
+        student.goals[goalIndex].evidence.push(newEvidence)
+        await student.save()
+        response.status(200).send(student)
+      } else {
+        response.status(404).send({ message: 'Goal not found.' })
+      }
+    } else {
+      response.status(404).send({ message: 'Student not found.' })
+    }
+  } catch (error) {
+    console.error(error)
+    response.status(500).send({ message: 'Error adding evidence.' })
+  }
+}
+
+  public async store({ request, response }: HttpContextContract) {
+    console.log(request.body())
+  
     try {
-      const data = request.only(['first_name', 'last_name', 'year'])
+      const data = request.only([
+        'first_name',
+        'last_name',
+        'year',
+        'behavior_score',
+        'goals',
+      ])
+  
+      if (!data.goals) {
+        data.goals = JSON.stringify([])
+      }
+  
       console.log(data)
       const student = await Student.create(data)
       response.status(201).send(student)
@@ -28,8 +104,8 @@ export default class StudentsController {
       console.error(error)
       response.status(500).send({ message: 'Error adding student.' })
     }
-  }
-
+  }  
+  
   public async show({}: HttpContextContract) {}
 
   public async edit({}: HttpContextContract) {}
