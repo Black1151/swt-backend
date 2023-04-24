1dc06259be1c18c7496fcfa47d7b4ddf7a5a2404
blackPrawn <danblack9988@gmail.com>
2023-04-24 14:15:34 +0100

    update Student model with fields for behaviour and goals

--- app/Models/Student.ts
+++ app/Models/Student.ts
@@ -1,19 +1,41 @@
-// app/Models/Student.ts
-
+import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export interface Goal {
+  title: string
+  date: string
+  status: string
+  evidence: Evidence[]
+}
+
+export interface Evidence {
+  date: string
+  name: string
+  image_link: string
+}
 
 export default class Student extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
   @column()
-  public first_name: string
+  public firstName: string
 
   @column()
-  public last_name: string
-  
-  @column()
-  public year: string
+  public lastName: string
 
-  // Add any other fields you need
+  @column()
+  public year: number
+
+  @column()
+  public behaviorScore: number
+
+  @column({ serializeAs: 'goals' })
+  public goals: Goal[]
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
 }
